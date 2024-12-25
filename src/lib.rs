0ed5287a2d4b6e8dcca7dd91e5119d368b41a742
tendguo <962203291@qq.com>
2024-12-25 21:25:19 +0800

    add some comments

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub use process::*;
 
 use enum_dispatch::enum_dispatch;
 
+/// https://blog.rust-lang.org/2023/12/21/async-fn-rpit-in-traits.html
 #[trait_variant::make(HttpService: Send)]
 #[enum_dispatch]
 pub trait CmdExecutor {
